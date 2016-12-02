4af865a2a5571cdfb2a3e3f2595bdaac4143bd56
diwaker_surya <diwaker.singh@games24x7.com>
2016-12-02 23:23:07 +0530

    initial commit

---
+++ app/src/main/java/com/i/apps/locationtracker/Requester.java
@@ -0,0 +1,29 @@
+package com.i.apps.locationtracker;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by diwaker on 14/10/16.
+ */
+
+public class Requester {
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
+
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
